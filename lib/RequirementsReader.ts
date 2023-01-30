c54a60091ccffe4bcb3cd85572dec87d73d6ad51
Rom Orlovich <romorlovich@gmail.com>
2023-01-30 17:53:22 +0200

    delete line in comments in RequirementsReader.ts

--- lib/RequirementsReader.ts
+++ lib/RequirementsReader.ts
@@ -3,16 +3,18 @@ import { CheerioDom } from './CheerioDom';
 import { Profile } from './Profile';
 
 export class RequirementsReader {
-  cheerioDom: CheerioDom;
-  elements: Cheerio<Element>[];
+  // cheerioDom: CheerioDom;
+  // elements: Cheerio<Element>[];
   profile: Profile;
-  constructor(profile: Profile, html: string, queryEl: string) {
-    this.cheerioDom = new CheerioDom(html);
-    this.elements = this.cheerioDom.toArray(queryEl);
+  constructor(profile: Profile) {
+    // this.cheerioDom = new CheerioDom(html);
+    // this.elements = this.cheerioDom.toArray(queryEl);
     this.profile = profile;
   }
-  private getSentences = () => {
-    const nodes = this.elements.filter((el) => {
+  getSentences = (html: string, queryEl: string) => {
+    const cheerioDom = new CheerioDom(html);
+    const elements = cheerioDom.toArray(queryEl);
+    const nodes = elements.filter((el) => {
       return el.text();
     });
     const sentences = nodes.map((el) =>
@@ -27,9 +29,9 @@ export class RequirementsReader {
   };
   private checkExcludeTech(word: string) {
     if (this.profile.getExcludeTech(word)) {
-      return false;
+      return true;
     }
-    return true;
+    return false;
   }
 
   private checkNumberIsLowerTheRange(numCheck: number | undefined, range: RegExpMatchArray) {
@@ -44,8 +46,7 @@ export class RequirementsReader {
     if (numCheck && yearNum > numCheck) return true;
   }
 
-  isJobValid() {
-    const sentences = this.getSentences();
+  isJobValid(sentences: string[][]) {
     for (let i = 0; i < sentences.length; i++) {
       const sentence = sentences[i];
       let yearsIndex = -1;
@@ -53,9 +54,10 @@ export class RequirementsReader {
       let languageMatch;
       for (let j = 0; j < sentence.length; j++) {
         const word = sentence[j];
-        // Check if the word is include in the excluded tech
 
-        this.checkExcludeTech(word);
+        // Check if the word is include in the excluded tech
+        if (this.checkExcludeTech(word)) return false;
+
         // Check a match of digit is already exist.
         if (!digitMatch) {
           digitMatch = word.match(/\d\d|\d-\d|\d/g);
@@ -64,20 +66,7 @@ export class RequirementsReader {
           // If it does check if the next word contains the word 'year'.
           if (digitMatch && j < sentence.length - 1 && sentence[j + 1].match(/year/)) {
             // Check if the match is range.
-            // if (digitMatch[0][1] === '-') {
-            //   const [min, max] = digitMatch[0].split('-');
-            //   if (
-            //     this.profile.overallEx &&
-            //     Number(min) > this.profile.overallEx
-            //   )
-            //     return false;
-            // }
             if (this.checkNumberIsLowerTheRange(this.profile.overallEx, digitMatch)) return false;
-            // if (
-            //   this.profile.overallEx &&
-            //   Number(digitMatch[0]) > this.profile.overallEx
-            // )
-            //   return false;
             // Check if the overallEx is smaller than digitMatch.
             if (this.checkDigitMatchIsBigger(this.profile.overallEx, digitMatch)) return false;
             yearsIndex = j;
@@ -91,15 +80,7 @@ export class RequirementsReader {
         // Check if there is language  and digit were match.
         if (languageMatch && digitMatch) {
           if (this.checkNumberIsLowerTheRange(languageMatch.max, digitMatch)) return false;
-          //   if (digitMatch[0][1] === '-') {
-          //     const [min, max] = digitMatch[0].split('-');
-          //     if (Number(min) > memo.max) return false;
-          //   }
-          //   const yearNum = Number(digitMatch[0]);
 
-          //   if (yearNum > memo.max) {
-          //     return false;
-          // }
           if (this.checkDigitMatchIsBigger(languageMatch.max, digitMatch)) return false;
           else {
             j = yearsIndex + 1;
@@ -111,5 +92,6 @@ export class RequirementsReader {
       digitMatch = null;
       languageMatch = undefined;
     }
+    return true;
   }
 }
