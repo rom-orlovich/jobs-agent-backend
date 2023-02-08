96acede12a5684899de8aab92e5a7554691b8300
Rom Orlovich <romorlovich@gmail.com>
2023-02-09 01:09:23 +0200

    continue the script that create positions hebrew name per alljob drushim and gotFriends

--- sandbox/positionNames.ts
+++ sandbox/positionNames.ts
@@ -15,15 +15,23 @@ export interface AllJobsCats {
   JobCount: number;
   ChildCategories: ChildCategory[];
 }
+export interface GotFriendsPositions {
+  checkboxProfessions: string;
+  radioAreas: string;
+}
+export interface AllPositionName {
+  regularName: string;
+  gotFriends: GotFriendsPositions;
+}
 
 (async () => {
   const gotFriendsCatArr = await ScanningFS.readJSON<GenericRecord<string>[]>(
-    path.join(__dirname, '..', 'JSON', 'gotFriends.json')
+    path.join(__dirname, 'JSON', 'gotFriends.json')
   );
   if (!gotFriendsCatArr) return;
 
   const gotFriendsCat = gotFriendsCatArr[0];
-  const gotFriendsPos: GenericRecord<{ checkboxProfessions: string; radioAreas: string }> = {};
+  const gotFriendsPos: GenericRecord<GotFriendsPositions> = {};
   Object.entries(gotFriendsCat).map(([catKey, catValue], i) => {
     Object.entries(gotFriendsCatArr[i + 1]).map(([postKey, postValue]) => {
       gotFriendsPos[postKey] = {
@@ -32,22 +40,33 @@ export interface AllJobsCats {
       };
     });
   });
+
+  // hebrewWords.forEach((el, i) => {
+  //   dictPositions[el] = { en: englishWords[i].trim(), he: el.trim() };
+  // });
+
   //   console.log(gotFriendsPos);
-  const cats = await ScanningFS.readJSON<AllJobsCats[]>(path.join(__dirname, '..', 'JSON', 'cat.json'));
+  const cats = await ScanningFS.readJSON<AllJobsCats[]>(path.join(__dirname, 'JSON', 'cat.json'));
   if (!cats) return;
-  const catAr: GenericRecord<any> = {};
+  const positionsData: GenericRecord<{
+    he: string;
+    gotFriends: GotFriendsPositions;
+  }> = {};
   for (const cat of cats) {
     cat.ChildCategories.forEach((el) => {
       const keywords = el.Keywords.split(',');
+
       keywords.forEach((el) => {
         const keyPos = el.trim();
-        catAr[keyPos] = {
-          regularName: keyPos,
+        positionsData[keyPos] = {
+          he: keyPos,
           gotFriends: gotFriendsPos[keyPos],
         };
       });
     });
   }
-  //   console.log(catAr);
-  //   console.log(catAr['מתכנת PHP']);
+
+  await ScanningFS.writeJSON(positionsData, path.join(__dirname, 'JSON', 'positionHeNames.json'));
+
+  // console.log(catAr['מנהל איכות']);
 })();
