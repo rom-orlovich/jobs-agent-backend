8e875c4172879b4976b9cb68a4839d2f81ef9455
Rom Orlovich <romorlovich@gmail.com>
2023-02-20 14:56:21 +0200

    create server with cluster architecture

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,24 +6,57 @@ import { MongoDBClient } from '../../lib/mongoClient';
 import { downloadResults, startScanner } from './controllers';
 import cors from 'cors';
 import { validateBeforeScanner } from './middleware';
+import cluster from 'node:cluster';
+import os from 'node:os';
+
+const totalCPUs = os.cpus().length;
 
 const app = Express();
 const PORT = 5000;
 
 export const mongoDB = new MongoDBClient();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.get('/api/jobs-agent/start/:userID', validateBeforeScanner, startScanner);
-app.get('/api/jobs-agent/download/:userID', validateBeforeScanner, downloadResults);
+const expressServer = () => {
+  // try {
+  //   await mongoDB.connect();
 
-(async () => {
-  try {
-    await mongoDB.connect();
+  app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+  app.get('/api/jobs-agent/start/:userID', validateBeforeScanner, startScanner);
+  app.get('/api/jobs-agent/download/:userID', validateBeforeScanner, downloadResults);
 
-    app.listen(5000, () => {
-      console.log(`server listen on port ${PORT}`);
-    });
-  } catch (error) {
-    await mongoDB.close();
+  console.log('start');
+  app.listen(5000, () => {
+    console.log(`server listen on port ${PORT}`);
+  });
+
+  // } catch (error) {
+  //   await mongoDB.close();
+  // }
+};
+
+const startClusters = async () => {
+  if (cluster.isPrimary) {
+    console.log(cluster);
+    console.log(`Number of CPUs is ${totalCPUs}`);
+    console.log(`Master ${process.pid} is running`);
+    try {
+      await mongoDB.connect();
+      // Fork workers.
+      for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+      }
+
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        console.log("Let's fork another worker!");
+        cluster.fork();
+      });
+    } catch (error) {
+      await mongoDB.close();
+    }
+  } else {
+    expressServer();
   }
-})();
+};
+
+startClusters();
