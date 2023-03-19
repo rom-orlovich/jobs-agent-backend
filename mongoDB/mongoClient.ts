e4224b4a5766823d0511ef24f05237619a4e6a06
Rom Orlovich <romorlovich@gmail.com>
2023-03-19 14:14:49 +0200

    refactor mongoClient.ts

--- mongoDB/mongoClient.ts
+++ mongoDB/mongoClient.ts
@@ -1,14 +1,10 @@
-import { Collection, Db, MongoClient } from 'mongodb';
-import { AnyFun, GenericRecord } from '../lib/types';
+import { MongoClient } from 'mongodb';
 
 export class MongoDBClient {
   client: MongoClient;
-  DBMap: GenericRecord<Db>;
-  collectionsMap: GenericRecord<Collection>;
+
   constructor() {
     this.client = new MongoClient(process.env.MONGO_DB_URI || '');
-    this.DBMap = {};
-    this.collectionsMap = {};
   }
   async connect() {
     return await this.client.connect();
@@ -16,33 +12,8 @@ export class MongoDBClient {
   async close() {
     return await this.client.close();
   }
-  getClient() {
-    return this.client;
-  }
+
   createDBcollection(dbName: string, collectionName: string) {
     return this.client.db(dbName).collection(collectionName);
-  }
-
-  createCollection(dbName: keyof typeof this.DBMap, collectionName: string) {
-    const db = this.getDB(dbName);
-    this.collectionsMap.collectionName = db.collection(collectionName);
-    return this.collectionsMap.collectionName;
-  }
-
-  getDB(dbName: keyof typeof this.DBMap) {
-    return this.DBMap[dbName];
-  }
-
-  getCollection(collectionName: keyof typeof this.collectionsMap) {
-    return this.collectionsMap[collectionName];
-  }
-
-  async mongoDBSetup(CB: AnyFun) {
-    try {
-      await this.connect();
-      await CB();
-    } catch (error) {
-      await this.close();
-    }
   }
 }
